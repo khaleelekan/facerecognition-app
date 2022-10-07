9fe468e81e043df49f7f77acff92aaf70834c4e9
unknown <kalilulekan@gmail.com>
2022-10-07 21:26:00 +0100

    first commit

---
+++ src/components/navigation/navigation.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './navigation.css'
+
+
+const Navigation = () => {
+    return(
+        <div className="nav">
+            <nav>Sign Out</nav>
+        </div>
+    )
+}
+
+export default Navigation;
