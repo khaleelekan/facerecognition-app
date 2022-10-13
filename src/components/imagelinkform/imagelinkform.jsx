57c57c89e7659f3eae566099cfe88bae21dc7e40
unknown <kalilulekan@gmail.com>
2022-10-13 23:56:23 +0100

    styles

--- src/components/imagelinkform/imagelinkform.jsx
+++ src/components/imagelinkform/imagelinkform.jsx
@@ -6,7 +6,11 @@ import './imagelinkform.css'
 const ImageLinkForm= () => {
     return(
         <div>
-            <p className="message">{'This app detects faces in your pictures. Give it a try'}</p>
+            <p className="message">This app detects faces in your pictures. Give it a try</p>
+            <div className="search-area">
+                <input type='text' />
+                <button>Detect</button>
+            </div>
         </div>
     )
 }
