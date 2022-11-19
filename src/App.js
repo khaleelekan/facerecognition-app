ed6921b6b9489265c15d74ea9c756894d4952624
unknown <kalilulekan@gmail.com>
2022-11-19 22:32:11 +0100

    development

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
  import React, { Component } from 'react';
  import './App.css'
  import Navigation from '../src/components/navigation/navigation' 
-import Logo from './components/logo/logo.component';
-import Rank from './components/Rank/rank.component'
-import ImageLinkForm from './components/imagelinkform/imagelinkform';
+ import Logo from './components/logo/logo.component';
+ import Rank from './components/Rank/rank.component'
+ import ImageLinkForm from './components/imagelinkform/imagelinkform';
 
 class App extends Component{
   constructor(){
@@ -15,18 +15,21 @@ class App extends Component{
   
   }
    onInputChange = (event) => {
-    console.log(event.target.value)
+   console.log(event.target.value)
  }
+  onSubmit = () => {
+    console.log('just clicked')
+  }
 
-  return()
-  {
+  render(){
+    return(
     <div>
        <Navigation />
        <Logo />
        <Rank />
-       <ImageLinkForm onInputChange={this.onInputChange} />
+    <ImageLinkForm onInputChange={this.onInputChange} onSubmit={this.onSubmit} /> 
     </div>
-  
+    )
   }
 }
 
--- src/components/imagelinkform/imagelinkform.jsx
+++ src/components/imagelinkform/imagelinkform.jsx
@@ -3,13 +3,13 @@ import './imagelinkform.css'
 
 
 
-const ImageLinkForm= ({ onInputChange }) => {
+const ImageLinkForm= ({ onInputChange, onSubmit }) => {
     return(
         <div>
             <p className="message">This app detects faces in your pictures. Give it a try</p>
             <div className="search-area">
-                <input type='text'  onChange={onInputChange}/>
-                <button>Detect</button>
+                <input type='text'  onChange={onInputChange} />
+                <button onClick={ onSubmit}>Detect</button>
             </div>
         </div>
     )
