72853cffe5cd2f22c8982c32290954f430d07ca6
unknown <kalilulekan@gmail.com>
2023-01-11 19:49:48 +0100

    addition to backend signin

--- src/components/Signin/Signin.jsx
+++ src/components/Signin/Signin.jsx
@@ -3,8 +3,8 @@ import React from "react";
 
 
 class SignIn extends React.Component {
-  constructor(){
-    super();
+  constructor(props){
+    super(props);
 
     this.state={
       signInEmail:'',
